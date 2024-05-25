344d3dd32a64a9f82dc74b3fc3186a55754cb555
Daniil Tsioma <126826909+daniiltsioma@users.noreply.github.com>
2024-05-25 01:45:51 -0700

    Create pub.py

---
+++ 08 maintenance/pub.py
@@ -0,0 +1,52 @@
+from google.cloud import pubsub_v1
+import json
+import os
+import subprocess
+from datetime import datetime
+
+# Function to fetch and return data for a vehicle ID 
+def fetch_bus_data(vehicle_id):
+    # Fetch JSON data using curl command
+    curl_command = f'curl https://busdata.cs.pdx.edu/api/getBreadCrumbs?vehicle_id={vehicle_id}'
+    json_data = json.loads(subprocess.check_output(curl_command, shell=True, text=True))
+
+    return json_data
+
+# Function to publish fetched data to Pub/Sub
+def publish_data(data, publisher, topic_path):
+    count = 0
+    # Iterate over each record in the JSON data
+    for record in data:
+        # Convert the record to a JSON string
+        record_str = json.dumps(record)
+        # Publish the JSON string as a message to Pub/Sub
+        future = publisher.publish(topic_path, record_str.encode("utf-8"))
+        if future:
+            count += 1
+
+        if count % 1000 == 0:
+            print(f"Published {count} messages.")
+
+    # print(f"Published {count} messages.")
+
+project_id = "buses-419920"
+topic_id = "archivetest"
+
+# Create Pub/Sub publisher
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id, topic_id)
+
+# List of vehicle IDs
+vehicle_ids = [
+    4212, 3802, 3249, 3507, 4234, 4227, 2938, 3929, 4036, 2907, 3007, 4528, 4237, 3131, 4052, 3616, 3959, 3634, 3328, 3264, 3218, 2916, 2920, 3748, 4205, 4001, 2903, 3624, 3631, 3110, 2908, 4202, 3031, 3518, 3226, 3022, 3262, 2936, 3626, 3525, 3245, 3615, 3720, 3201, 3153, 3958, 3932, 3253, 3324, 2930, 3921, 3543, 3524, 3223, 3010, 3001, 4020, 3318, 3241, 3509, 3718, 3647, 3123, 3267, 3413, 3157, 3639, 3213, 4010, 3515, 2902, 3151, 2918, 3013, 4529, 3122, 3027, 4024, 3221, 3549, 3136, 3142, 3158, 3404, 3236, 4216, 3202, 3711, 3502, 3015, 3134, 3713, 3531, 3127, 3619, 4209, 4207, 3163, 3629, 3736
+
+    # Add more vehicle IDs for project (read:ALL)
+]
+
+# Iterate over each vehicle ID
+for vehicle_id in vehicle_ids:
+    # Fetch data for the vehicle ID
+    bus_data = fetch_bus_data(vehicle_id)
+
+    # Publish the fetched data to Pub/Sub
+    publish_data(bus_data, publisher, topic_path)
